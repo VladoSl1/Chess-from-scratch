86cb64e8ba90adec16a2120b76cfa57680a6d23c
VladoSl1 <55553612+VladoSl1@users.noreply.github.com>
2023-02-15 22:31:53 +0100

    Add files via upload

--- Libraries/std_extension.h
+++ Libraries/std_extension.h
@@ -1,36 +1,49 @@
 #pragma once
 
 #include <set>
+#include <map>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
 
-
-static std::vector<std::string> splitStr(const std::string& str, const std::string& delim = " ")
+namespace string
 {
-	std::vector<std::string> res;
-	std::string::size_type pos, lastPos = 0, length = str.length();
-
-	while (lastPos < length + 1)
+	static std::vector<std::string> splitStr(const std::string& str, const std::string& delim = " ")
 	{
-		pos = str.find_first_of(delim, lastPos);
-		if (pos == std::string::npos)			// not found
-			pos = length;
+		std::vector<std::string> res;
+		std::string::size_type pos, lastPos = 0, length = str.length();
 
-		if (pos != lastPos)
-			res.push_back(std::string(str.data() + lastPos, pos - lastPos));
+		while (lastPos < length + 1)
+		{
+			pos = str.find_first_of(delim, lastPos);
+			if (pos == std::string::npos)			// not found
+				pos = length;
 
-		lastPos = pos + 1;
+			if (pos != lastPos)
+				res.push_back(std::string(str.data() + lastPos, pos - lastPos));
+
+			lastPos = pos + 1;
+		}
+
+		return res;
 	}
-
-	return res;
 }
+
 
 namespace dict
 {
 	template<typename Key, typename Val>
 	static std::vector<Key> keys(const std::unordered_map<Key, Val>& m)
+	{
+		std::vector<Key> k(m.size());
+		std::transform(m.begin(), m.end(), k.begin(), [](std::pair<Key, Val> pair) { return pair.first; });
+
+		return k;
+	}
+	
+	template<typename Key, typename Val>
+	static std::vector<Key> keys(const std::map<Key, Val>& m)
 	{
 		std::vector<Key> k(m.size());
 		std::transform(m.begin(), m.end(), k.begin(), [](std::pair<Key, Val> pair) { return pair.first; });
@@ -45,6 +58,23 @@ namespace dict
 		std::transform(m.begin(), m.end(), v.begin(), [](std::pair<Key, Val> pair) { return pair.second; });
 
 		return v;
+	}
+
+	template<typename Key, typename Val>
+	static std::vector<Val> values(const std::map<Key, Val>& m)
+	{
+		std::vector<Val> v(m.size());
+		std::transform(m.begin(), m.end(), v.begin(), [](std::pair<Key, Val> pair) { return pair.second; });
+
+		return v;
+	}
+}
+
+namespace vector
+{
+	template<typename T>
+	static bool isIn(const std::vector<T>& v, const T& val) {
+		return std::find(v.begin(), v.end(), val) != v.end();
 	}
 }
 
